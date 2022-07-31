743b6a4eb384943e389cb15da8b0b074dfb0ca21
islomRahimov <110152566+islomRahimov@users.noreply.github.com>
2022-07-31 11:35:41 +0500

    Add files via upload

---
+++ untitled10/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        if (a>0){
+            System.out.println("musbat");
+        }else if (a == 0){
+            System.out.println(1);
+        }else  {
+            System.out.println("manfiy");
+
+
+        }
+    }
+}
